b172a10773518706853d75ad208e582305f3a406
waste143 <masihmalakooti12345@gmail.com>
2016-10-10 16:14:35 +0330

    helpen.lua

--- plugins/helpen.lua
+++ plugins/helpen.lua
@@ -13,7 +13,7 @@ do
 
 function run(msg, matches)
   return [[ 
-💠Jove English Help V4.0💠
+💠iran Guard English Help V4.0💠
 
 🔆Customization SuperGroup:🔆
 💭lock
@@ -69,8 +69,8 @@ function run(msg, matches)
 🔏setlink
 🏳link
 〰〰〰〰〰〰〰
-☑️ #Jove V4.0
-💠Titan Zeus💠
+☑️ #iranGuard V4.0
+💠masih malakooti💠
 ]]
 end
 
